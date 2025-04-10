e35ffe97dd4df582b4f5effd659f84fa4b377c69
SardorbekToxtasinov <sardorbektoxtasinov28@gmail.com>
2025-04-10 15:01:43 +0500

    NewsApi

--- api/views/bookmark.py
+++ api/views/bookmark.py
@@ -1,22 +1,18 @@
-# Django Rest Framework'dan kerakli importlarni qilamiz
-from rest_framework.viewsets import ModelViewSet  # ModelViewSetni import qilamiz
-from rest_framework import permissions  # Xavfsizlik ruxsatlarini import qilamiz
-from core.models.bookmark import *  # Bookmark modelini import qilamiz
-from api.serializers.bookmark import *  # Bookmark serializatorini import qilamiz
-
-# Faqat 'Super Admin' va 'server' foydalanuvchilarga ruxsat beradigan maxsus permission sinfi
-class IsServerOrSuperAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        # Agar foydalanuvchi superadmin bo'lsa yoki username 'server' bo'lsa, ruxsat beriladi
-        if request.user.is_superuser or request.user.username == 'server':
-            return True
-        return False
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from core.models.bookmark import Bookmark
+from api.serializers.bookmark import BookmarkSerializer
 
 # Bookmark viewset'i - bu modelga oid barcha CRUD amallarini bajarish uchun ishlatiladi
 class BookmarkViewSet(ModelViewSet):
-    queryset = Bookmark.objects.all()  # Barcha bookmark'larni olish
+    queryset = Bookmark.objects.all() # Barcha bookmark'larni olish
     serializer_class = BookmarkSerializer  # Serializator sifatida BookmarkSerializer ishlatiladi
-    permission_classes = [IsServerOrSuperAdmin]  # Faqat maxsus ruxsatga ega foydalanuvchilar uchun
+    permission_classes = [IsAuthenticated]  # Faqat autentifikatsiyalangan foydalanuvchilarga ruxsat beriladi
+
+    # Faqat foydalanuvchiga tegishli bookmarklarni olish
+    def get_queryset(self):
+        # Faqat o'ziga tegishli bookmarklar ko'rsatiladi
+        return Bookmark.objects.filter(user=self.request.user)
 
     # Yangilikni yaratishdan oldin, foydalanuvchining identifikatorini qo'shish
     def perform_create(self, serializer):
